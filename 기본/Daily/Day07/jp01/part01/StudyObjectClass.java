1c2d8b13204719528182ccf2cfad96136beb768b
unknown <ellen10735@gmail.com>
2024-01-20 09:04:43 +0900

    addDailySourceCode

---
+++ 기본/Daily/Day07/jp01/part01/StudyObjectClass.java
@@ -0,0 +1,17 @@
+//package jb01.part01;
+
+public class StudyObjectClass
+{
+
+	public String toString(){		
+      return "no : "+no+" name : " +name+" 인 A class 입니다.";
+    }
+
+
+
+	public static void main(String[] args) 
+	{
+
+
+	}//end of main
+}//end of class
